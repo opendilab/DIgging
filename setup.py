3bf9f3c6d8c04584c16db2be70f07dbff4a92fda
Robin Chen <sissure@qq.com>
2022-08-22 11:36:57 +0800

    fix test bug

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'torch<=1.8',
+        'torch<=1.10',
         'di-engine>=0.3',
         'scikit-learn>=0.18.0',
         'protobuf<=3.20.1',  # for di-engine 0.3.1 bug
