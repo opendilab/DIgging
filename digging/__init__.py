5861d070112a5fa59553531eab699ad59b8092e8
RobinC94 <sissure@qq.com>
2022-09-01 23:10:59 +0800

    add version number

--- digging/__init__.py
+++ digging/__init__.py
@@ -1,5 +1,5 @@
 __TITLE__ = "DIgging"
-__VERSION__ = "0.0.0"
+__VERSION__ = "0.0.0.1"
 __DESCRIPTION__ = "Decision AI searching and optimization platform"
 __AUTHOR__ = "OpenDILab Contributors"
 __AUTHOR_EMAIL__ = "opendilab.contact@gmail.com"
